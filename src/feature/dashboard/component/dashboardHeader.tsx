15b3ffd04346621f02da98af03965e2b5320c97f
devam-Scaletech <devam.chaudhari@scaletech.xyz>
2023-06-13 23:18:33 +0530

    feat: added profile picture | dynamic greeting message

--- src/feature/dashboard/component/dashboardHeader.tsx
+++ src/feature/dashboard/component/dashboardHeader.tsx
@@ -1,7 +1,7 @@
 const DashboardHeader = () => {
 	return (
 		<div className='width--full flex justify-content--center mt--35'>
-			<p className='website__name font-size--30 font--bold'>
+			<p className='website__name font-size--30 font--bold text--neon-green'>
 				Fitness Revolution<sup>Tm</sup>
 			</p>
 		</div>
--- src/feature/dashboard/component/userProfile.tsx
+++ src/feature/dashboard/component/userProfile.tsx
@@ -1,7 +1,41 @@
+import { useCallback, useEffect, useState } from 'react';
+import profilePic from 'assets/image/profilePic.jpg';
 const UserProfile = () => {
+	const [greeting, setGreeting] = useState('');
+
+	const greetingMessage = useCallback(() => {
+		const currentTime = new Date().getHours();
+		if (currentTime >= 0 && currentTime < 12) {
+			setGreeting('Good Morning');
+		} else if (currentTime >= 12 && currentTime < 18) {
+			setGreeting('Good Afternoon');
+		} else {
+			setGreeting('Good Evening');
+		}
+	}, []);
+
+	useEffect(() => {
+		greetingMessage();
+	}, []);
+
 	return (
 		<div className='width--full'>
-			<div className='profile__box border-radius--25  flex flex--column' />
+			<div className='profile__box border-radius--25  flex '>
+				<div className='flex justify-content--center align-items--center width--35 p--25 '>
+					<img
+						src={profilePic}
+						alt='profile picture'
+						width={250}
+						height={350}
+						className='border-radius--25'
+					/>
+				</div>
+				<div className='flex flex--column'>
+					<div className='mt--20'>
+						<p className='font-size--50 text--neon-green font--semi-bold'>{greeting}, John</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
