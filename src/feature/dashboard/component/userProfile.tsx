18d59f7559f393105910dc9bd88117e768931ad2
devam-Scaletech <devam.chaudhari@scaletech.xyz>
2023-06-13 14:58:29 +0530

    cont: designing user profile details box

---
+++ src/feature/dashboard/component/userProfile.tsx
@@ -0,0 +1,9 @@
+const UserProfile = () => {
+	return (
+		<div className='width--full'>
+			<div className='profile__box border-radius--25  flex flex--column' />
+		</div>
+	);
+};
+
+export default UserProfile;
--- src/feature/dashboard/container/dashboardContainer.tsx
+++ src/feature/dashboard/container/dashboardContainer.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from 'react';
 import DashboardHeader from '../component/dashboardHeader';
+import UserProfile from '../component/userProfile';
 import 'assets/styles/module/dashboard.scss';
 const DashboardContainer = () => {
 	return (
 		<Fragment>
 			<DashboardHeader />
+			<UserProfile />
 		</Fragment>
 	);
 };
